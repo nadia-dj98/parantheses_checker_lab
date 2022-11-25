e0cf3c8c32d529fc897730aaed1ed93eda5e26be
Nadia <nadia_dj@hotmail.co.uk>
2022-11-25 08:46:24 +0000

    attempted another method but wasn't able to complete

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -4,12 +4,13 @@ public class ParenthesesChecker {
 
     private Stack<Character> parenthesesStackOpenBrackets;
     private Stack<Character> parenthesesStackClosedBrackets;
+    private Stack<Character> parenthesesStack;
 
 
     public ParenthesesChecker()  {
         this.parenthesesStackOpenBrackets = new Stack<>();
         this.parenthesesStackClosedBrackets = new Stack<>();
-
+        this.parenthesesStack = new Stack<>();
     }
 
     public boolean checkParentheses(String testString) {
@@ -37,6 +38,36 @@ public class ParenthesesChecker {
 
 
     }
+
+      //second attempt
+
+//    public boolean checkParentheses(String testString){
+//
+////        int lengthOfString = testString.length();
+////
+////        System.out.println(lengthOfString);
+//
+//        for(int i = 0; i< testString.length(); i++) {
+//
+//            char letter = testString.charAt(i);
+//
+//            if( letter == '(' || letter == '{' || letter == '[' || letter == '<')
+//                parenthesesStack.push(letter);
+//
+//            else if (letter == ')' || letter == '}' || letter == ']' || letter == '>') {
+//                if(parenthesesStack.empty()){
+//                    return false;
+//                } else {
+//                    parenthesesStack.pop();
+//                }
+//
+//            }
+//
+//        }
+//
+//        return true;
+//    }
+
 
 
 }
